3cdd470c9c42ebd40093cb7cd2e0f3ec27b49ffd
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-10-19 21:58:19 +0530

    fix: correct import statement for database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-import connectDB from './utils/db';
+import connectDB from './utils/db.js';
 
+
+dotenv.config("./");
 const app = express();
 
+app.use(cors({
+    origin:"http://localhost:5173"
+}));  
+app.use(express.json());
+
+app.use("/api/auth", )
+app.use("/api/v1/key", );
+app.use("/api/v1/project", )
+
+connectDB().then(()=>{
+app.listen(5001, () => {
+    console.log("Server is running on port: ", 5001);
+});
+});
