61b9ebfa0d07e957371433833e50024b980a9c2f
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-10-20 16:47:27 +0530

    feat: update Key model schema to replace project_name with projectID reference

--- backend/src/Models/Key.js
+++ backend/src/Models/Key.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 const keySchema = mongoose.Schema({
     userId : {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    project_name : {type : String , required : true},
+    projectID : {type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true },
     api_key : {type: String, required: true},
-});
+},
+{timestamps  : true}
+);
 
 
 const Key = mongoose.model("Key", keySchema);
