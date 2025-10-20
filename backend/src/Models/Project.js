c647397ac2522f01953e32a923c087bbf536cbb8
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-10-20 16:47:31 +0530

    feat: remove api_keys field from Project model schema

--- backend/src/Models/Project.js
+++ backend/src/Models/Project.js
@@ -3,12 +3,7 @@ import mongoose from 'mongoose';
 const projectSchema = mongoose.Schema({
     userId : {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     project_name : {type: String, required: true},
-    api_keys : [
-          {
-            api_key_id: { type: mongoose.Schema.Types.ObjectId, ref: "Key", required: true }
-          },
-        ],
-    },
+},
     {timestamps: true}
 );
 
