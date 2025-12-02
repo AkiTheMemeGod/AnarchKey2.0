6352c927c7e2ec6daddd3ccda12d08b0670fd7d3
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-12-02 18:30:42 +0530

    feat: add keys field to project schema for enhanced project management

--- backend/src/Models/Project.js
+++ backend/src/Models/Project.js
@@ -3,10 +3,20 @@ import mongoose from 'mongoose';
 const projectSchema = mongoose.Schema({
     userId : {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     project_name : {type: String, required: true},
+    keys: {
+        type: [
+            {
+                key_name: { type: String, required: true },
+                api_key: { type: String, required: true },
+                createdAt: { type: Date, default: Date.now }
+            }
+        ],
+        required: false,
+        default: []
+    }
 },
     {timestamps: true}
 );
-
 
 const Project = mongoose.model("Project", projectSchema);
 
