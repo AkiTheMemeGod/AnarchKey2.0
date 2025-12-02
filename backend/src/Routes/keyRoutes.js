ba25aa0fd1bc71f26715d48d104f62e74f7a79e8
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-12-02 18:28:50 +0530

    feat: add authentication middleware to key routes for improved security

--- backend/src/Routes/keyRoutes.js
+++ backend/src/Routes/keyRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
+import authMiddleware from '../middleware/authmiddleware.js';
 import {getAllKeys, getOneKey, addKey, updateKey, deleteKey} from '../Controllers/keyControllers.js';
 const router = express.Router();
 
 
-router.get("/getkeys", getAllKeys);
-router.get("/:id", getOneKey)
-router.post("/", addKey);
-router.delete("/:id", deleteKey)
-router.put("/:id", updateKey)
+router.get("/getkeys", authMiddleware, getAllKeys);
+router.get("/:id", authMiddleware, getOneKey)
+router.post("/", authMiddleware, addKey);
+router.delete("/:id", authMiddleware, deleteKey)
+router.put("/:id", authMiddleware, updateKey)
 
 export default router;
