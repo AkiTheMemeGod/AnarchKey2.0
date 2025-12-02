0df6ab6e82f3e9aa19ca777f9fcbab04df2d4eab
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-12-02 18:28:44 +0530

    feat: add authentication middleware to project routes for enhanced security

--- backend/src/Routes/projectRoutes.js
+++ backend/src/Routes/projectRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
+import authMiddleware from '../middleware/authmiddleware.js';
 
 import {getAllProjects, getOneProject, addProject, deleteProject, updateProject} from '../Controllers/projectControllers.js'
 const router = express.Router();
 
 
-router.get("/getprojects", getAllProjects);
-router.get("/:id", getOneProject)
-router.post("/", addProject);
-router.delete("/:id", deleteProject)
-router.put("/:id", updateProject)
+router.get("/getprojects", authMiddleware, getAllProjects);
+router.get("/:id", authMiddleware, getOneProject)
+router.post("/", authMiddleware, addProject);
+router.delete("/:id", authMiddleware, deleteProject)
+router.put("/:id", authMiddleware, updateProject)
 
 export default router;
