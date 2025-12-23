a093c06047f724f1d5e0785064d1260815a0967f
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-12-23 20:37:49 +0530

    feat: organize server routes and improve formatting in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,22 +7,24 @@ import connectDB from './utils/db.js';
 import authRoutes from './Routes/authRoutes.js';
 import keyRoutes from './Routes/keyRoutes.js';
 import projectRoutes from './Routes/projectRoutes.js';
+import sdkRoutes from './Routes/sdkRoutes.js';
 
 
 
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173"
-}));  
+    origin: "http://localhost:5173"
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/v1/key", keyRoutes);
 app.use("/api/v1/project", projectRoutes)
+app.use("/api/v1/sdk", sdkRoutes);
 
-connectDB().then(()=>{
-app.listen(5001, () => {
-    console.log("Server is running on port: ", 5001);
-});
+connectDB().then(() => {
+    app.listen(5001, () => {
+        console.log("Server is running on port: ", 5001);
+    });
 });
