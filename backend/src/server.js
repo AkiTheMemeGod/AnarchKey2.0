5c773dbb9e32d0910effdbe8a400bc9e723a5f79
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-12-24 12:41:18 +0530

    feat: Update server configuration to use environment variables for frontend URL and port

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,13 +8,13 @@ import authRoutes from './Routes/authRoutes.js';
 import keyRoutes from './Routes/keyRoutes.js';
 import projectRoutes from './Routes/projectRoutes.js';
 import sdkRoutes from './Routes/sdkRoutes.js';
-
+dotenv.config();
 
 
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: process.env.FRONTEND_URL || "http://localhost:5173"
 }));
 app.use(express.json());
 
@@ -22,9 +22,9 @@ app.use("/api/auth", authRoutes)
 app.use("/api/v1/key", keyRoutes);
 app.use("/api/v1/project", projectRoutes)
 app.use("/api/v1/sdk", sdkRoutes);
-
+const port = process.env.PORT
 connectDB().then(() => {
-    app.listen(5001, () => {
-        console.log("Server is running on port: ", 5001);
+    app.listen(port, () => {
+        console.log("Server is running on port: ", port);
     });
 });
