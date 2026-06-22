5c760266abd42aa07c35620f8d6f08da49f947b9
AkiTheMemeGod <k.akashkumar@gmail.com>
2026-06-22 22:05:03 +0530

    Feature : New routes to add bulk key imports from a file

--- backend/src/Routes/keyRoutes.js
+++ backend/src/Routes/keyRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authMiddleware from '../middleware/authmiddleware.js';
-import { getAllKeys, getOneKey, addKey, updateKey, deleteKey, getUsageStats, getUsageLogs } from '../Controllers/keyControllers.js';
+import { getAllKeys, getOneKey, addKey, updateKey, deleteKey, bulkAddKeys, getUsageStats, getUsageLogs } from '../Controllers/keyControllers.js';
 const router = express.Router();
 
 
@@ -8,6 +8,7 @@ router.get("/getkeys", authMiddleware, getAllKeys);
 router.get("/usage", authMiddleware, getUsageStats);
 router.get("/logs", authMiddleware, getUsageLogs);
 router.get("/:id", authMiddleware, getOneKey)
+router.post("/bulk", authMiddleware, bulkAddKeys);
 router.post("/", authMiddleware, addKey);
 router.delete("/:id", authMiddleware, deleteKey)
 router.put("/:id", authMiddleware, updateKey)
